979b11f737c8bb5446b0b0aa5d80fa51f1f8a5c0
CandyX22 <114142536+CandyX22@users.noreply.github.com>
2022-09-22 20:56:15 +0700

    Add files via upload

---
+++ [064002200012] - [CANDY SOEKA WIYONO] - latihan 1.py
@@ -0,0 +1,10 @@
+#Candy soeka wiyono - 064002200012
+print ("nama kamu siapa?")
+x = input()
+print ("hallo selamat siang," + x)
+print ("dari mana asal kamu candy?")
+y = input()
+print ("Wah dari," + y)
+print("Sebelumnya bersekolah dimana?")
+z = input()
+print('Pasti menyenangkan bersekokah di,' + z)
