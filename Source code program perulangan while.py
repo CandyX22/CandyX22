f6cdd1de0bdb224434e12b0b355474e5b42e1975
Candy W <114142536+CandyX22@users.noreply.github.com>
2022-10-13 22:41:03 +0700

    Add files via upload

---
+++ Source code program perulangan while.py
@@ -0,0 +1,7 @@
+#CANDY SOEKA WIYONO_(064002200012)
+#Program menggunakan perulangan while #CANDY SOEKA WIYONO_(064002200012)
+number =int(input("masukan jumlah maksimal ")) 
+for i in range(number, 0, -1):
+    for j in range (i) :
+        print(i, end='')        
+    print('\r')
